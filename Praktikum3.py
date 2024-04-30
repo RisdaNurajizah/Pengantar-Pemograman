8a86e72bf5e9024c1aac0e299424f1c2545f0efa
RisdaNurajizah <risdanurajizah603@gmail.com>
2024-04-30 10:54:46 +0700

    commit 1

---
+++ Praktikum3.py
@@ -0,0 +1,39 @@
+#memasukan data dalam sebuah variable
+name = "Risda" #isi variable berupa string
+print(name) #mencetak variable
+
+#nilai dan tipe data dalam variable
+age = 18 #tipe data angka / numerik
+print(age) #mencetak nilai age
+print(type(age)) #melihat tipe data dari age
+age = "delapan belas" #tipe data string
+print(age) #mencetak string dari age
+print(type(age)) #melihatr tipe data
+
+first_name = "Risda"
+middle_name = "Nur"
+last_name = "Ajizah"
+name = first_name+" "+middle_name+" "+last_name
+age = 19
+hobby = "Tidur"
+print("Profil\n",name,"\n",age,"\n",hobby)
+
+#contoh variable lainnya
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 8
+print(age,Age,aGe,AGE,a_g_e,_age,age_,_AGE_) #mencetak variable
+
+numberofcollegegraduates = 1945
+NUMBEROFCOLLEGEGRADUATES = 1945
+numberOfCollegeGraduates = 1945
+NumberOfCollegeGraduates = 1945
+number_of_college_graduates = 1945
+#mencetak variable
+number_of_college_graduates = 1945
+print(numberofcollegegraduates,NUMBEROFCOLLEGEGRADUATES,numberOfCollegeGraduates,NumberOfCollegeGraduates,)
